f3f6f79905520332dc02e6fa25560219988fd2a8
Facundo <salasfacundo96@gmail.com>
2023-02-24 02:50:42 -0300

    Se finalizo validacion y recoleccion de butacas

--- js/butacas.js
+++ js/butacas.js
@@ -1,12 +1,1 @@
-function butacas(id) {
-    let elemento = document.getElementById(id);
 
-    if(elemento.className == "disponible"){
-        elemento.className = "elegido";
-        return;
-    }
-    if(elemento.className == "elegido"){
-        elemento.className = "disponible";
-        return;
-    }             
-}
--- js/formulario.js
+++ js/formulario.js
@@ -1,7 +1,9 @@
 let form = document.querySelector('.formulario');
 let barraProgreso = document.querySelectorAll('.barra__option');
 
-form.addEventListener('click', function(e) {
+form.addEventListener('click', formControllers);
+
+function formControllers(e) {
     let elemento = e.target;
     let isButtonNext = elemento.classList.contains('step__button--next');
     let isButtonBack = elemento.classList.contains('step__button--back');
@@ -29,4 +31,65 @@ form.addEventListener('click', function(e) {
         pasoActual.classList.add('inactive');
         cambiarPaso.classList.remove('inactive');
     }
-});
+}
+
+buildSeats();
+
+function buildSeats(){
+    let butacas = document.querySelector('.butacas');
+    let filas = "";
+
+    for (let i = 1; i <= 10; i++) {
+        filas += `<div class="fila">`;
+        for (let j = 1; j <= 15; j++) {  
+            filas += `
+                <div class=${(Math.random() < 0.5) ? "disponible" : "ocupado"} id="${i}-${j}"  onclick="butacas(id)"></div>
+            `;
+        } 
+        filas +=  `</div>`
+    }    
+    filas += `<div id="seatValidate" class="d-flex justify-content-center mt-5 text-danger"></div>`
+    butacas.innerHTML = filas;
+    let botonConfirmar = document.querySelector('#butacasConfirmar');
+    botonConfirmar.addEventListener("click", seatValidator)
+    
+}
+
+function butacas(id) {
+    let elemento = document.getElementById(id);
+
+    if(elemento.className == "disponible"){
+        elemento.className = "elegido";
+        return;
+    }
+    if(elemento.className == "elegido"){
+        elemento.className = "disponible";
+        return;
+    }             
+}
+
+function seatValidator(e){
+    let butacas = document.querySelectorAll(".elegido");
+    let seatValidate = document.querySelector('#seatValidate');
+    if(butacas.length == 0){
+        seatValidate.innerHTML = "Elija algun asiento";
+        e.stopImmediatePropagation();
+    }else{
+        seatValidate.innerHTML = "";
+        chosenSeats();
+    }
+}
+
+function chosenSeats(){
+    let butacas = document.querySelectorAll(".elegido");
+    let resultado = "";
+
+    for (let i = 0; i < butacas.length; i++) {        
+        resultado += butacas[i].id + ","
+    }
+
+    console.log("RESULTADO");
+    console.log(resultado);
+}
+
+
