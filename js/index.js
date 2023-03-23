6720a461b08f26cccf6ff0024a09d6f674c160e5
Facundo <salasfacundo96@gmail.com>
2023-03-23 00:58:37 -0300

    Ahora las imagenes de cartelera se cargan desde una api

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const open = document.getElementsByClassName('open');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 
+
 var keys = ['Avatar: El camino del agua', 'Alerta extrema', 'Babilonia', 'Faelman', '1985', 'Llaman a la puerta'] ;
 
 var values = [
@@ -13,9 +14,9 @@ var values = [
             "Mientras vacacionan en una cabaña remota, una pequeña niña y sus padres son tomados como rehenes por cuatro extraños armados, quienes exigen que la familia tome una decisión impensable para evitar el apocalipsis. Con acceso limitado al mundo exterior, la familia deberá decidir en qué creen antes de que todo se pierda."
         ]
 
+        consumeApi();
 let keyMap = {};
 builKeyMap();
-
 close.addEventListener('click', () => {
     modal_container.classList.remove('show');
 });
@@ -47,4 +48,73 @@ function builKeyMap(){
             modal_container.classList.add('show');
         });
     }
+}
+
+function consumeApi() {
+    let movies;
+    let genres;
+
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=a813ce03ea202b120e2307c4325bd6c3&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
+        .then(res => res.json())
+        .then(responseMovies => {
+            fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=a813ce03ea202b120e2307c4325bd6c3&language=es-ES")
+                .then(res => res.json())
+                .then(responseGenres => {
+                    movies = responseGenres;
+                    fillCarousel(responseGenres, responseMovies)
+                });
+        });
+}
+
+function fillCarousel(responseGenres, responseMovies){
+
+    const firstPage = document.getElementById('firstPage')
+    const secondPage = document.getElementById('secondPage')
+    let firstPageHtml = "";
+    let secondPageHtml = "";
+
+    results = responseMovies.results.slice(0,8);
+
+    inicio = results.slice(0, 4);
+    final = results.slice(4);
+
+    console.log(responseMovies)
+    for (let i = 0; i < 4; i++) {
+        firstPageHtml += `
+        <div class="cardPremiere open">
+            <img class=" img-fluid" src="https://image.tmdb.org/t/p/w500/${inicio[i].poster_path}" alt="First slide">
+            <small class="movieTitle">${inicio[i].title}</small>
+            <small><i class="fa fa-star"></i><label>${findGenreById(responseGenres, inicio[i].genre_ids[0])}</label></small>
+        </div>
+    `
+    }
+
+
+for (let i = 0; i < 4; i++) {
+    secondPageHtml += `
+    <div class="cardPremiere open">
+            <img class=" img-fluid" src="https://image.tmdb.org/t/p/w500/${final[i].poster_path}" alt="First slide">
+            <small class="movieTitle">${final[i].title}</small>
+            <small><i class="fa fa-star"></i><label>${findGenreById(responseGenres, final[i].genre_ids[0])}</label></small>
+        </div>
+`
+}
+
+firstPage.innerHTML = firstPageHtml;
+secondPage.innerHTML = secondPageHtml;
+
+}
+
+
+function findGenreById(responseGenres, id){
+    let respuesta;
+    responseGenres.genres.forEach(e => {
+        if(e.id == id){     
+            console.log("aca2")
+            console.log( e.name)
+            console.log("aca2")
+            respuesta= e.name;
+        }
+    })
+    return respuesta;
 }
